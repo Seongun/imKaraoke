c4a75e65059e18902a2ebb89c4c71931e3901fbf
Seongun <ss8355@nyu.edu>
2017-11-10 23:44:44 +0400

    working version. With a few limitations 1. only 1/3 of the lyrics are displayed by the api 2. need to input user warnings

--- scripts.js
+++ scripts.js
@@ -2,46 +2,59 @@
   var lyricsReady=false;
   var showBackground= false;
   var songPlaying=false;
-  var updateLyricFunc= null;
-
+  var updateLyricId= null;
   var isShowingSearchScreen = true;
-
+  var pitches=[];
   var title, artist;
   var lyricsParsed = [];
   var counter = 0;
   var lyricElement = $("#lyrics");
+  var speedElement = $("#speed");
   var mic, fft;
+  var speed =3000;
+ 
 
 
 
 $(document).ready(function(){
+
+    for(var i=0; i<1024; i++){
+      pitches[i]= new Scribble();
+    }
+
     searchReady=true;
-    $("#singingScreen").hide();
+
+
 });
 
 
 
 function switchView(){
+
   counter = 0;
 
   if(isShowingSearchScreen){
+
     $("#singingScreen").show();
     $("#searchScreen").hide();
     isShowingSearchScreen=false;
-    updateLyricFunc = updateLyric;
 
   }else{
+
     $("#singingScreen").hide();
     $("#searchScreen").show();
-
-    if(updateLyricFunc){
-        clearInterval(updateLyricFunc);
-        updateLyricFunc=null;
-    }
-    lyricElement.html("");
+    $("#startLyricsButton").show();
 
     isShowingSearchScreen=true;
+
+    if(updateLyricId){
+        clearInterval(updateLyricId);
+        updateLyricId=null;
+    }
+    lyricElement.html( "" );
+    speedElement.html("");
     songPlaying=false;
+    showBackground=false;
   }
 
 }
@@ -49,11 +62,17 @@ function switchView(){
 
 
 function newSearch(){	
+   $("#returnButton").css("opacity", "1");
+
 
   lyricsParsed = [];
 
-  if(!searchReady) alert("give us a minute to load");
-  
+  if(!searchReady){
+    alert("give us a minute to load");
+    return null;
+  }else{
+
+
    lyricsReady=false;
 
    title = $("#songSearch").val();
@@ -65,41 +84,43 @@ function newSearch(){
 
   }else{
 
-   $.ajax({
-    type: "GET",
-    data: {
-        apikey:"246e71cd28a61ff6770ac93ec828e024",
-        q_artist: artist,
-        q_track: title,
-        format:"jsonp",
-        callback:"jsonp_callback"
-    },
-    url: "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
-    dataType: "jsonp",
-    jsonpCallback: 'jsonp_callback',
-    contentType: 'application/json',
+     $.ajax({
+      type: "GET",
+      data: {
+          apikey:"246e71cd28a61ff6770ac93ec828e024",
+          q_artist: artist,
+          q_track: title,
+          format:"jsonp",
+          callback:"jsonp_callback"
+      },
+      url: "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
+      dataType: "jsonp",
+      jsonpCallback: 'jsonp_callback',
+      contentType: 'application/json',
 
-    success: function(data) {
+      success: function(data) {
 
-        if(data.message.body.lyrics){
+          if(data.message.body.lyrics){
 
-          var lyric = data.message.body.lyrics.lyrics_body; 
-          parseLyrics(lyric);
-          switchView();
-        }else{
+            var lyric = data.message.body.lyrics.lyrics_body; 
+            parseLyrics(lyric);
+            switchView();
 
-          alert("no such song "+title+" by "+ artist+ "!");
+          }else{
 
-        }
-   
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-    }    
-  });
+            alert("no such song "+title+" by "+ artist+ "!");
 
+          }
+     
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR);
+          console.log(textStatus);
+          console.log(errorThrown);
+      }    
+      });
+
+    }
   }
 
 }
@@ -111,7 +132,6 @@ function parseLyrics(lyric){
    lyric = lyric.slice(0,n);
   
    lyricsParsed = lyric.split("\n");
-   
    lyricsParsed[ lyricsParsed.length - 1 ] = "End of Lyrics";
    
    for(var i=0; i<lyricsParsed.length; i++){
@@ -128,6 +148,8 @@ function parseLyrics(lyric){
 
 
 function startLyrics(){
+  
+    $("#returnButton").css("opacity", "");
 
     if(!lyricsParsed){
       alert("lyric not ready yet");
@@ -138,19 +160,27 @@ function startLyrics(){
       console.log("song is already playing");
       return 0;
     }
+    lyricElement.html("Get Ready, lyrics are loading soon. Press <- -> to adjust lyrics speed!");
+    speedElement.html("Speed: " + (6000-speed).toString() );
 
     songPlaying=true;
     showBackground=true;
-
+    speed = 3000;
     counter=0;
-    updateLyricFunc.call();
-    setInterval(updateLyricFunc, 3000);
+
+
+    if(updateLyricId){
+        clearInterval(updateLyricId);
+        updateLyricId=null;
+    }
+    updateLyricId = setInterval(updateLyric, speed);
+    $("#startLyricsButton").hide();
 
 }
 
 
-
 function updateLyric() {
+  console.log("updateLyric");
 
   lyricElement.html( lyricsParsed[counter] );
 
@@ -159,7 +189,12 @@ function updateLyric() {
   if (counter >= lyricsParsed.length) {
   
      lyricsReady= false;
-
+     showBackground=false;
+     if(updateLyricId){
+       clearInterval(updateLyricId);
+     }
+     updateLyricFunc=null;
+     console.log("we're done");
   }
 
 }
@@ -193,14 +228,84 @@ function setup() {
 
 }
 
+function keyPressed() {
+  if(updateLyricId){
+
+      if (keyCode === LEFT_ARROW && songPlaying) {
+            console.log("changing speed");
+
+          speed +=100;
+           clearInterval(updateLyricId);
+           updateLyricId = setInterval(updateLyric, speed);
+          speedElement.html("Speed: " + (6000-speed).toString() );
+
+      } else if (keyCode === RIGHT_ARROW) {
+            console.log("changing speed");
+
+          speed -=100;  
+          clearInterval(updateLyricId);
+          updateLyricId = setInterval(updateLyric, speed);
+               speedElement.html("Speed: " + (6000-speed) );
+
+      }
+
+     
+
+  }
+  
+}
+
 
 
 function draw() {
 
-  if( showBackground ){
+       var r = random(50);
+      //  var g,b;
+      // background(r*5);
+       var g = random(50);
+       var b = random(50);
+       background(r*5, b*5, g*5);
+    if(showBackground){
+      
 
-     background(255);
      var spectrum = fft.analyze();
+     var temp=0;
+
+     for(var i=0; i<spectrum.length; i++){
+          
+
+        temp+=spectrum[i];
+
+        if(i%3==0){
+           r = random(windowWidth);
+           g = random(windowHeight);
+           if(i%2==0){
+
+             pitches[i].scribbleEllipse( r, g, b, temp);
+
+           }else{
+
+             pitches[i].scribbleEllipse( r, g, temp, b);
+
+           }
+        }
+        if(i%4==0){
+
+           r = random(50);
+           g = random(50);
+           b = random(50);
+
+           pitches[i].scribbleLine(i * windowWidth/1024, windowHeight, i* windowWidth/1024, windowHeight - temp );
+          
+           stroke(r*5, g*5, b*5);
+           var w = random(4);
+           strokeWeight(w);
+           temp=0;
+        } 
+
+       // pitches[i].scribbleLine(i * windowWidth/1024, windowHeight, windowWidth/1024, windowHeight- spectrum[i]*50);
+     }
+     // scribble.scribbleLine( x1, y1, x2, y2 );
 
      // for(var i=0; i< spectrum.length; i++){
 
@@ -208,10 +313,12 @@ function draw() {
 
 
      // }
-  }
+  
 
  
-
+   }else{
+    background(255);
+   }
 }
 
 
